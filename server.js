24823fee07379503e4e0313bf763e7d78097042e
kaidornel <kai_dornel@hotmail.com>
2020-09-14 21:08:58 -0400

    created new files and folders

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json())
+app.use('/', routes);
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+
+
+
+
